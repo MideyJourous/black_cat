d83d4a0bcd79e34926af0bb64a86fa9515c6ca37
MideyJourous <rlwkdkmj@gmail.com>
2025-03-19 23:40:50 +0900

    first commit

---
+++ lib/features/authentication/widgets/birthday_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/email_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+
+class BirthdayScreen extends StatefulWidget {
+  const BirthdayScreen({super.key});
+
+  @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+  DateTime initialDate = DateTime.now();
+  DateTime requiredDate = DateTime(
+    DateTime.now().year - 12,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => EmailScreen()));
+  }
+
+  void _setTextFieldDate(DateTime newDateTime) {
+    setState(() {
+      initialDate = newDateTime;
+      final textDate = initialDate.toString().split(" ").first;
+      _birthdayController.value = TextEditingValue(text: textDate);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(elevation: 0, title: Text("Sign Up")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            Text(
+              "Create Username",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            Text(
+              "Your birthday won't be shown publicly",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.withAlpha(20)),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.withAlpha(20)),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v28,
+            GestureDetector(
+              onTap: _onNextTap,
+              child: FormButton(disabled: false),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 200,
+        child: BottomAppBar(
+          child: CupertinoDatePicker(
+            minimumDate: requiredDate,
+            initialDateTime: initialDate,
+            maximumDate: initialDate,
+            mode: CupertinoDatePickerMode.date,
+            onDateTimeChanged: _setTextFieldDate,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/widgets/form_button.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class FormButton extends StatelessWidget {
+  const FormButton({super.key, required this.disabled});
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        padding: EdgeInsets.symmetric(vertical: Sizes.size16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.size5),
+          color:
+              disabled
+                  ? Colors.grey.withAlpha(50)
+                  : Theme.of(context).primaryColor,
+        ),
+        duration: Duration(milliseconds: 300),
+        child: AnimatedDefaultTextStyle(
+          duration: Duration(milliseconds: 300),
+          style: TextStyle(
+            color: disabled ? Colors.grey.withAlpha(40) : Colors.white,
+            fontWeight: FontWeight.w400,
+          ),
+          child: Text("Next", textAlign: TextAlign.center),
+        ),
+      ),
+    );
+  }
+}
